2a353af67b2d8845238e01eaf17e78543cf9bbe4
bhunu <mabhunure@gmail.com>
2026-05-08 15:57:58 +0200

    Refactor ALB configuration: move ALB listener resource to correct position in alb.tf

--- infra/alb.tf
+++ infra/alb.tf
@@ -16,6 +16,18 @@ resource "aws_lb" "app" {
   }
 }
 
+# ALB Listener
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
+
 // Target Group for the ALB
 resource "aws_lb_target_group" "app" {
   name     = "${var.project_name}-tg"
@@ -42,14 +54,3 @@ resource "aws_lb_target_group" "app" {
   }
 }
 
-# ALB Listener
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.app.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app.arn
-  }
-}
